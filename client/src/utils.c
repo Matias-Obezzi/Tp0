ea9923ce1f24c09e886e15a1b941a7b79d62d397
Matias Obezzi <matiasobezzi@gmail.com>
2021-08-29 00:58:37 -0300

    Cambio configuracion del hints | client

--- client/src/utils.c
+++ client/src/utils.c
@@ -22,9 +22,8 @@ int crear_conexion(char *ip, char* puerto)
 	struct addrinfo *server_info;
 
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
 
 	getaddrinfo(ip, puerto, &hints, &server_info);
 
